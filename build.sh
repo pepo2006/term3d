49eec6acd938b40b8167569880357661620d57db
pepo2006 <pedroignacio.isern@gmail.com>
2023-03-22 14:08:31 -0300

    cleaning up a little bit

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gcc -lm -Wall -Wextra -ggdb -o demo main.c
+if ! ls bin; then mkdir bin; fi 2>/dev/null
+
+gcc -lm -Wall -Wextra -ggdb -o ./bin/demo main.c
--- main.c
+++ main.c
@@ -128,123 +128,127 @@ vec2_t project_p3(vec3_f p, vec3_f c, vec3_f c_a, vec3_f e){
     return b;
 }
 
-//int main(){
-//    struct termios old_settings, settings;
-//
-//    tcgetattr(STDIN_FILENO, &old_settings);
-//    settings = old_settings;
-//
-//    settings.c_lflag &= (~ICANON & ~ECHO);
-//
-//    tcsetattr(STDIN_FILENO, TCSANOW, &settings);
-//
-//    
-//
-//    clear_screen();
-//
-//    size_t screen_size = WIDTH*HEIGHT;
-//    char *screen = malloc(screen_size*sizeof(char));
-//    memset(screen, ' ', screen_size*sizeof(char));
-//
-//    char c = '#';
-//
-//    //1   3
-//    // 5 7
-//    // 4 6
-//    //0   2
-//
-//    vec3_f camera          = { 0, 0, -30 };
-//    vec3_f angles          = { 0, 0,  0  };
-//    vec3_f display_surface = { 0, 0,  20 };
-//
-//    vec3_f vertices[] = {{ -10, -10,  10 }, { -10,  10,  10 },
-//                         {  10, -10,  10 }, {  10,  10,  10 },
-//                         { -10, -10, -10 }, { -10,  10, -10 },
-//                         {  10, -10, -10 }, {  10,  10, -10 }};
-//
-//    pair_t edges[] = {{ 0, 1 }, { 3, 1 }, { 5, 1 }, { 6, 2 }, 
-//                      { 0, 2 }, { 3, 2 }, { 5, 4 }, { 6, 4 }, 
-//                      { 0, 4 }, { 3, 7 }, { 5, 7 }, { 6, 7 }}; 
-////    float ang_x = PI/32,
-////          ang_y = PI/32,
-////          ang_z = PI/32;
-//
-//    for(;;){
-////        angles.y -= PI/32;
-////        angles.x -= PI/32;
-////        angles.z -= PI/32;
-//
-////        if(angles.y < 2*PI) angles.y += 2*PI;
-////        if(angles.x < 2*PI) angles.x += 2*PI;
-////        if(angles.z < 2*PI) angles.z += 2*PI;
-//
-//        vec2_t vertices_p[8]; 
-//
-//        for(int i = 0; i < 8; i++){
-//            vertices_p[i] = project_p3(vertices[i], camera, angles, display_surface);
-//        }
-//
-//        for(int i = 0; i < 12; i++){
-//            put_line2(screen, vertices_p[edges[i].fst].x,
-//                              vertices_p[edges[i].fst].y,
-//                              vertices_p[edges[i].snd].x,
-//                              vertices_p[edges[i].snd].y, c);
-//        }
-//
-//        clear_screen();
-//        print_buff(screen, screen_size);
-//        fflush(stdout);
-//        memset(screen, ' ', screen_size);
-//
-//        vec3_f camera_local = { 0, 0, 0 };
-//
-//        switch(getc(stdin)){
-//            case 'w':
-//                camera_local.z =  1;
-//                break;
-//            case 'a':
-//                camera_local.x = -1;
-//                break;
-//            case 's':
-//                camera_local.z  = -1;
-//                break;
-//            case 'd':
-//                camera_local.x  =  1;
-//                break;
-//            case '\x1b': 
-//                getc(stdin);
-//                switch(getc(stdin)){
-//                    case 'A':
-//                        angles.x -= PI/32;
-//                        break;
-//                    case 'B':
-//                        angles.x += PI/32;
-//                        break;
-//                    case 'C':
-//                        angles.y += PI/32;
-//                        break;
-//                    case 'D':
-//                        angles.y -= PI/32;
-//                        break;
-//                    default:
-//                        break;
-//                }
-//                break;
-//            default:
-//                break;
-//        }
-//
-//        sum_vec3(camera, rotate_p3(camera_local, scale_vec3(angles, -1)), &camera);
-//
-//    }
-//
-//    tcgetattr(STDIN_FILENO, &old_settings);
-//
-//    return 0;
-//}
+#if 1
+
+int main(){
+    struct termios old_settings, settings;
+
+    tcgetattr(STDIN_FILENO, &old_settings);
+    settings = old_settings;
+
+    settings.c_lflag &= (~ICANON & ~ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &settings);
+
+    
+
+    clear_screen();
+
+    size_t screen_size = WIDTH*HEIGHT;
+    char *screen = malloc(screen_size*sizeof(char));
+    memset(screen, ' ', screen_size*sizeof(char));
+
+    char c = '#';
+
+    //1   3
+    // 5 7
+    // 4 6
+    //0   2
+
+    vec3_f camera          = { 0, 0, -30 };
+    vec3_f angles          = { 0, 0,  0  };
+    vec3_f display_surface = { 0, 0,  20 };
+
+    vec3_f vertices[] = {{ -10, -10,  10 }, { -10,  10,  10 },
+                         {  10, -10,  10 }, {  10,  10,  10 },
+                         { -10, -10, -10 }, { -10,  10, -10 },
+                         {  10, -10, -10 }, {  10,  10, -10 }};
+
+    pair_t edges[] = {{ 0, 1 }, { 3, 1 }, { 5, 1 }, { 6, 2 }, 
+                      { 0, 2 }, { 3, 2 }, { 5, 4 }, { 6, 4 }, 
+                      { 0, 4 }, { 3, 7 }, { 5, 7 }, { 6, 7 }}; 
+//    float ang_x = PI/32,
+//          ang_y = PI/32,
+//          ang_z = PI/32;
+
+    for(;;){
+//        angles.y -= PI/32;
+//        angles.x -= PI/32;
+//        angles.z -= PI/32;
+
+//        if(angles.y < 2*PI) angles.y += 2*PI;
+//        if(angles.x < 2*PI) angles.x += 2*PI;
+//        if(angles.z < 2*PI) angles.z += 2*PI;
+
+        vec2_t vertices_p[8]; 
+
+        for(int i = 0; i < 8; i++){
+            vertices_p[i] = project_p3(vertices[i], camera, angles, display_surface);
+        }
+
+        for(int i = 0; i < 12; i++){
+            put_line2(screen, vertices_p[edges[i].fst].x,
+                              vertices_p[edges[i].fst].y,
+                              vertices_p[edges[i].snd].x,
+                              vertices_p[edges[i].snd].y, c);
+        }
+
+        clear_screen();
+        print_buff(screen, screen_size);
+        fflush(stdout);
+        memset(screen, ' ', screen_size);
+
+        vec3_f camera_local = { 0, 0, 0 };
+
+        switch(getc(stdin)){
+            case 'w':
+                camera_local.z =  1;
+                break;
+            case 'a':
+                camera_local.x = -1;
+                break;
+            case 's':
+                camera_local.z  = -1;
+                break;
+            case 'd':
+                camera_local.x  =  1;
+                break;
+            case '\x1b': 
+                getc(stdin);
+                switch(getc(stdin)){
+                    case 'A':
+                        angles.x -= PI/32;
+                        break;
+                    case 'B':
+                        angles.x += PI/32;
+                        break;
+                    case 'C':
+                        angles.y += PI/32;
+                        break;
+                    case 'D':
+                        angles.y -= PI/32;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+
+        sum_vec3(camera, rotate_p3(camera_local, scale_vec3(angles, -1)), &camera);
+
+    }
+
+    tcgetattr(STDIN_FILENO, &old_settings);
+
+    return 0;
+}
 
 // WIDTH 40
 // HEIGHT 40
+
+#else
 
 int main(void){
     size_t screen_size = WIDTH*HEIGHT;
@@ -259,3 +263,4 @@ int main(void){
 
     return 0;
 }
+#endif
